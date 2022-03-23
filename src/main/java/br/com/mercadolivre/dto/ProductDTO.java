f83b02785cd55d37709cf6c244217a8314e3276f
Andris Buscariolli <andris.buscariolli@mercadolivre.com>
2022-03-23 17:21:54 -0300

    add class ProductDTO

--- src/main/java/br/com/mercadolivre/dto/ProductDTO.java
+++ src/main/java/br/com/mercadolivre/dto/ProductDTO.java
@@ -1,5 +1,26 @@
 package br.com.mercadolivre.dto;
 
-public class ProductDTO {
+import br.com.mercadolivre.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ProductDTO {
+    private Integer id;
+    private String name;
+    private Integer quantity;
+
+    public ProductDTO convertToDTO(Product product) {
+        return new ProductDTO(
+                product.getId(),
+                product.getName(),
+                product.getQuantity()
+        );
+    }
 }
