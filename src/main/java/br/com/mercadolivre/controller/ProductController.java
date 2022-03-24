5fddb669936b7b607736e420b3c7bf7a4658a803
jbarcelos <joao.barcelos@mercadolivre.com>
2022-03-24 09:02:42 -0300

    feat add response code

--- src/main/java/br/com/mercadolivre/controller/ProductController.java
+++ src/main/java/br/com/mercadolivre/controller/ProductController.java
@@ -5,26 +5,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.mercadolivre.dto.RequestListProductDTO;
 import br.com.mercadolivre.dto.ProductDTO;
+import br.com.mercadolivre.dto.RequestListProductDTO;
 import br.com.mercadolivre.model.Product;
 import br.com.mercadolivre.service.ProductService;
 
 @RestController
 public class ProductController {
-	
-	@Autowired 
+
+	@Autowired
 	public ProductService service;
-	
+
 	@PostMapping("/insert-products-request")
-	public void create(@RequestBody RequestListProductDTO products) throws IOException {
-		
-		List<Product> collect = products.getProducts().stream().map(e -> ProductDTO.productToDTO(e)).collect(Collectors.toList());
+	public ResponseEntity<Object> create(@RequestBody RequestListProductDTO products) throws IOException {
+
+		List<Product> collect = products.getProducts().stream().map(e -> ProductDTO.productToDTO(e))
+				.collect(Collectors.toList());
 		service.create(collect);
-		
+
+		return new ResponseEntity<Object>(HttpStatus.CREATED);
+
 	}
 }
--- src/main/java/br/com/mercadolivre/service/ProductService.java
+++ src/main/java/br/com/mercadolivre/service/ProductService.java
@@ -11,13 +11,12 @@ import br.com.mercadolivre.util.FileUtils;
 @Service
 public class ProductService {
 
-	private static final String JSON_PRODUCTS_PATH = "./products";
+	private static final String JSON_PRODUCTS_PATH = "./products.json";
 
 	public void create(List<Product> products) throws IOException {
 		FileUtils<Product> fileUtils = new FileUtils<>();
 		List<Product> list = fileUtils.readObjectsFromFile(JSON_PRODUCTS_PATH);
 		list.addAll(products);
-
 		fileUtils.writeObjectToFile(list, JSON_PRODUCTS_PATH);
 	}
 
